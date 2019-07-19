c1a1a644d0e5d52089e790b83088b36fc65b8c98
Kamil Paczkowski <37217644+ShamanDev@users.noreply.github.com>
2019-07-19 13:30:17 +0200

    Add files via upload

--- Models/RoomData.cs
+++ Models/RoomData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,14 @@ namespace RoomManagement1.Models
 {
     public class RoomData
     {
-        public int Id { get; set; }
+        [Required(ErrorMessage = "Numer sali jest wymagany")]
+        [Range(0, int.MaxValue, ErrorMessage = "This value must be a number")]
         public int Numer { get; set; }
+
+        [Required(ErrorMessage = "Opis jest wymagany")]
+        [DataType(DataType.Text)]
         public string Opis { get; set; }
-        public bool Zajęte { get; set; }
+        public bool Zajete { get; set; }
         public string Godzina { get; set; }
     }
 }
