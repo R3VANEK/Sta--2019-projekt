b497b07771b31a1bd6488bba9257c2fbb7bb9091
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-07-24 11:05:32 +0200

    Add files via upload

---
+++ Models/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SohbiRoomManager.Models
+{
+    public class UserData
+    {
+        [Required(ErrorMessage = "Login jest wymagany")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        public string Password { get; set; }
+
+        public static string Acceptedlogin { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SohbiRoomManager.Models
+{
+    public class Users
+    {
+        public IList<UserData> users { get; set; }
+    }
+}
